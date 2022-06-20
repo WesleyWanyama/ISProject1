2c9794c832e4208e45bb33c2e42f22462ed09b35
WesleyWanyama <83776940+WesleyWanyama@users.noreply.github.com>
2022-06-20 22:58:49 +0300

    ff

--- View/rider_homepage.php
+++ View/rider_homepage.php
@@ -6,14 +6,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Rider Homepage</title>
 	<link href="rider_homepage.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
 
-	<!-- ADD Rider DETAILS -->
-  <div class="rider_info">
-
-    
-  </div>
 
   <!-- ======= Header ======= -->
   <header id="header" class="d-flex align-items-center">
